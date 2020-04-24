5a8caa05d528f1c4e80375acc2cc768a417d08f3
dipankarshaw <dipcom02323@gmail.com>
2020-04-24 23:24:19 +0530

    loop shell added

--- loop.sh
+++ loop.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 a=1
 b=25
- until [ $a -ge 750 ]
+ until [ $a -ge 100 ]
  do
- ansible-playbook EE_ELAN.yml -e '{"FF_start_evi_ID":'$a',"FF_end_evi_ID":'$b'}' --tags create_config
+ ansible-playbook EE_ELAN.yml -e '{"FF_start_evi_ID":'$a',"FF_end_evi_ID":'$b'}' --tags create_service
+ a=`expr $a + 25`
+ b=`expr $b + 25` 
+ done
+
+a=1
+b=25
+ until [ $a -ge 100 ]
+ do
+ ansible-playbook EE_ELAN.yml -e '{"FF_start_evi_ID":'$a',"FF_end_evi_ID":'$b'}' --tags delete_service
  a=`expr $a + 25`
  b=`expr $b + 25` 
  done
